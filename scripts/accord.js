7fe729e8846db86ec8ee56000bc7000344b64e46
for-theme <dlya.spama219@gmail.com>
2025-11-15 20:44:29 +0100

    Delete scripts/accord.js

--- scripts/accord.js
+++
@@ -1,6 +0,0 @@
-document.querySelectorAll('.faq__question').forEach(question => {
-    question.addEventListener('click', () => {
-        const item = question.parentNode;
-        item.classList.toggle('active');
-    });
-});
